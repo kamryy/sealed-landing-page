89e3a232dbc8282e458ef5ed53bf6cb417026e5d
MaciekPon <m.ponicki1995@gmail.com>
2026-05-25 19:06:29 +0200

    change page from algo to partners

--- src/app/algo/page.tsx
+++
@@ -1,5 +0,0 @@
-import ComingSoon from "@/components/gates/ComingSoon";
-
-export default function AlgoPage() {
-  return <ComingSoon title="Algo" />;
-}
---
+++ src/app/partners/page.tsx
@@ -0,0 +1,5 @@
+import ComingSoon from "@/components/gates/ComingSoon";
+
+export default function PartnersPage() {
+  return <ComingSoon title="Partners" />;
+}
